0f80513e6088a0dc5bd948800753265d9640212d
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-20 16:08:28 -0500

    feat: :construction: Se genera validaciones para el partido y la insercion de nuevo partido.
    Se generan validaciones de existencia y la insercion de un nuevo partido en la base de datos.

--- js/modules/convocatoria.js
+++ js/modules/convocatoria.js
@@ -26,50 +26,5 @@ export class convocatoria extends connect {
     }
 
 
-    async registerGame({equipos,id_partido,id_estadio }){
-        let res;
-        try {
-            //verificar la existencia del partido en la base de datos
-            const partidoExist=await this.db.collection('partido').findOne({_id:new ObjectId(id_partido)})
-            if(!partidoExist){
-                return{
-                    error: "Not found",
-                    message: "El partido no existe"
-                }
-            }
-
-            //verificar la existencia del estadio en la base de datos
-            let estadioExist=await this.db.collection('estadio').findOne({_id: new ObjectId(id_estadio)})
-            if(!estadioExist){
-                return{
-                    error: "Not found",
-                    message: "El estadio no existe"
-                }
-            }
-
-
-            //verificar la existencia de equipo en la base de datos
-            let equipoExist= await Promise.all(equipos.map(async id=> {
-                let equipoExist= await this.db.collection('equipo').findOne({_id: new ObjectId(id)})
-                return equipoExist
-
-            }))
-            if(!equipoExist.every(equipo=> equipo)){
-                return{
-                    error:"Not found",
-                    message: "Uno o los dos equipos no existe"
-                };
-            }
-
-
-
-            
-
-
-
-
-        } catch (error) {
-            return { error: "Error", message: error.message,details: error.errInfo};
-        }
-    }
+   
 }
--- js/modules/partido.js
+++ js/modules/partido.js
@@ -1,4 +1,5 @@
 import { connect } from "../../helpers/db/connect.js";
+import { ObjectId } from "mongodb";
 
 export class partido extends connect {
     static instancePartido;
@@ -23,4 +24,65 @@ export class partido extends connect {
         await this.conexion.close();
         return res;
     }
+
+    async registerGame({equipolocal,equipoVisitante,fecha_y_hora,id_estadio,tipo }){
+        let res;
+        try {
+            //verificar la existencia del equipolocal en la base de datos
+            const equipolocalExist=await this.db.collection('equipo').findOne({_id:new ObjectId(equipolocal)})
+            if(!equipolocalExist){
+                return{
+                    error: "Not found",
+                    message: "El equipo local no existe"
+                }
+            }
+
+            //verificar la existencia del equipoVisitante en la base de datos
+            let equipoVisitanteExist=await this.db.collection('equipo').findOne({_id: new ObjectId(equipoVisitante)})
+            if(!equipoVisitanteExist){
+                return{
+                    error: "Not found",
+                    message: "El equipo visitante no existe"
+                }
+            }
+
+            //verificar si el estadio existe
+            let estadioExist=await this.db.collection('estadio').findOne({_id: new ObjectId(id_estadio)})
+            if(!estadioExist){
+                return{
+                    error: "Not found",
+                    message: "El estadio no existe"
+                }
+            }
+
+            //verificar que el equipo local y el equipo visitante no sean el mismo
+            if(equipolocalExist._id.toString()===equipoVisitanteExist._id.toString()){
+                return{
+                    error: "Not valid",
+                    message: "Los equipos no pueden ser el mismo"
+                }
+            }
+
+            //insercion del nuevo partido.
+                let nuevoGame= {
+                    equipolocal:new ObjectId(equipolocal),
+                    equipoVisitante: new ObjectId(equipoVisitante),
+                    fecha_y_hora:new Date(fecha_y_hora),
+                    id_estadio: new ObjectId(id_estadio),
+                    tipo: tipo
+                }
+                res= await this.collection.insertOne(nuevoGame);
+                return {
+                    message: "Partido registrado correctamente",
+                    data: res.ops
+                };
+    
+
+        } catch (error) {
+            return { error: "Error", message: error.message,details: error.errInfo};
+            
+        }
+    }
+
+
 }
