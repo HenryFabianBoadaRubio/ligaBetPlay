09027a8ef4be6c23df19f0ced4d7c115d82562df
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-21 17:18:28 -0500

    feat: :construction: Se comienza el caso de uso 22.
    Se ingresa la validacion de existencia del jugador , formato de la fecha y la verificacion de que el premio no este asignado.

--- js/modules/premio.js
+++ js/modules/premio.js
@@ -1,4 +1,5 @@
 import { connect } from "../../helpers/db/connect.js";
+import { ObjectId } from "mongodb";
 
 export class premio extends connect {
     static instancePremio;
@@ -22,5 +23,42 @@ export class premio extends connect {
         const res = await this.collection.find({}).toArray(); 
         await this.conexion.close();
         return res;
+    }
+
+    async registerPrize({nombre,descripcion,fecha,id_jugador}){
+        let res;
+        try {
+            //verificar la existencia del jugador en la base de datos
+            const jugadorExist=await this.db.collection('jugador').findOne({_id:new ObjectId(id_jugador)})
+            if(!jugadorExist){
+                return{
+                    error: "Not found",
+                    message: "El jugador no existe"
+                }
+            }
+
+            //verificar el formato de la fecha
+            const dateFormat = /^\d{4}-\d{2}-\d{2}$/;
+            if(!dateFormat.test(fecha)){
+                return{
+                    error: "Not valid",
+                    message: "Formato de fecha incorrecto, debe ser AA-MM-DD"
+                }
+            }
+
+            //verificar si el premio ya existe para el jugador
+            const premioExist=await this.db.collection('premio').findOne({id_jugador:new ObjectId(id_jugador), nombre})
+            if(premioExist){
+                return{
+                    error: "Not valid",
+                    message: "El premio ya existe para el jugador"
+                }
+            }
+
+            
+            
+        } catch (error) {
+            return { error: "Error", message: error.message,details: error.errInfo};
+        }
     }
 }
