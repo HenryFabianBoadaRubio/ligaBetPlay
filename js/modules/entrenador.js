36a2bc25537679182ce91bd0699f8109eea3d1b8
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-21 09:36:49 -0500

    feat: :construction: Se comienza el caso de uso 7.
    Se ingresa la validacion de existencia de entrenado por medio del nombre .

--- js/modules/entrenador.js
+++ js/modules/entrenador.js
@@ -23,4 +23,37 @@ export class entrenador extends connect {
         await this.conexion.close();
         return res;
     }
+
+    async registerCoach({nombre, edad, nacionalidad, id_equipo, experiencia}){
+        let res;
+        try {
+            
+            //verificar si el entrenador no existe ya.
+            const coachExist = await this.collection.findOne({ nombre });
+            if (coachExist) {
+                return {
+                    error: "Not valid",
+                    message: "El entrenador ya existe"
+                };
+            }
+
+            //insercion del nuevo entrenador.
+            let nuevoCoach= {
+                nombre,
+                edad,
+                nacionalidad,
+                id_equipo: new ObjectId(id_equipo),
+                experiencia
+            }
+            res= await this.collection.insertOne(nuevoCoach);
+            return {
+                message: "Entrenador registrado correctamente",
+                data: res.ops
+            };
+
+        } catch (error) {
+            return { error: "Error", message: error.message,details: error.errInfo};
+            
+        }
+    }
 }
