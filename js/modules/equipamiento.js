5547a4c949e943f2a744cf91b7569c574cfd9e8a
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-21 13:43:00 -0500

    feat: :construction: Se comienza el caso de uso 20.
    Se comienza el desarrollo del caso de uso 20 agregando la verificacion de equipo y insertando un try-catch para los errores.

--- js/modules/equipamiento.js
+++ js/modules/equipamiento.js
@@ -23,4 +23,29 @@ export class equipamiento extends connect {
         await this.conexion.close();
         return res;
     }
+
+    async registerEquipment({id_equipo,tipo,cantidad,fecha_adquisicion}){
+        let res;
+        try {
+            //verificar la existencia del equipo.
+            const equipoExist=await this.db.collection('equipo').findOne({_id:new ObjectId(id_equipo)})
+            if(!equipoExist){
+                return{
+                    error: "Not found",
+                    message: "El equipo no existe"
+                }
+            }
+
+
+            //registrar el equipamiento.
+            const nuevoEquipamiento= {
+                id_equipo: new ObjectId(id_equipo),
+                tipo: tipo,
+                cantidad: cantidad,
+                fecha_adquisicion: new Date(fecha_adquisicion)
+            }
+        } catch (error) {
+            
+        }
+    }
 }
