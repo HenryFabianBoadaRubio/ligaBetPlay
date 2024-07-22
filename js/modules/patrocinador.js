0626dc6ff3688aca656459a88512b3f3d9e1cf29
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-07-22 03:15:06 -0500

    feat: :construction: Se comienza el caso de uso 24.
    Se comienza el caso de uso 24 se realiza la validacion del formato de hora y la validacion para el monto del patrocinio siendo mayor de 0.

--- js/modules/patrocinador.js
+++ js/modules/patrocinador.js
@@ -23,4 +23,25 @@ export class patrocinador extends connect {
         await this.conexion.close();
         return res;
     }
+
+    async registerSponsor({nombre,tipo,monto,fechaInicio,fechaFin}){
+        let res;
+        try {
+            //Verificar el formato de fechas que sea AA-MM-DD
+            const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+            if (!dateRegex.test(fechaInicio) ||!dateRegex.test(fechaFin)) {
+                throw new Error("Formato de fecha inválido. Debe ser AA-MM-DD");
+            }
+
+            //Validar que el monto sea un número mayor a cero
+            if (typeof monto!== 'number' || monto <= 0) {
+                throw new Error("El monto debe ser un número mayor a cero");
+            }
+            
+        } catch (error) {
+            return { error: "Error", message: error.message,details: error.errInfo};
+            
+        }
+    }
+
 }
