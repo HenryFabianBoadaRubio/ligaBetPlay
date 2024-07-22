eb47a5b3a354a528afd3647dbbddc2bddeca3b40
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-22 01:01:06 -0500

    feat: :bookmark: Finalización caso 2 "V.1"
    Se da inicio a la realizacion del caso de uso 2
    
    feat: :construction: Creacion de registro de jugador
    
    Se generó el registro del jugador
    
    feat: :construction: Creacion de actualizar y eliminar jugador
    
    Se generaron los aprtados de edicion y eliminacion del jugador
    
    feat: :construction: Creacion de la ejecucion del caso 2 en el archivo main
    
    Se generó la ejecucion del caso 2 en el archivo main
    
    feat: :bug: Arreglo de error al ejecutar la expresion regular
    
    Se generó un arreglo en la expresión regular

--- js/modules/jugador.js
+++ js/modules/jugador.js
@@ -1,9 +1,11 @@
-import { connect } from "../../helpers/db/connect.js";
+import { ObjectId } from 'mongodb';
+import { connect } from '../../helpers/db/connect.js';
 
 export class jugador extends connect {
     static instanceJugador;
     db;
     collection;
+
     constructor() {
         if (jugador.instanceJugador) {
             return jugador.instanceJugador;
@@ -13,14 +15,60 @@ export class jugador extends connect {
         this.collection = this.db.collection('jugador');
         jugador.instanceJugador = this;
     }
-    destructor(){
+
+    destructor() {
         jugador.instanceJugador = undefined;
         connect.instanceConnect = undefined;
     }
-    async getAllTest() {
+
+    async registrarJugador(jugadorData) {
         await this.conexion.connect();
-        const res = await this.collection.find({}).toArray(); 
+        const jugadorExistente = await this.collection.findOne({ nombre: {$regex: new RegExp(`^${jugadorData.nombre}$`, 'i')}});
+        if (jugadorExistente) {
+            await this.conexion.close();
+            throw new Error("El jugador ya existe");
+        }
+
+        const nuevoJugador = {
+            _id: new ObjectId(jugadorData._id),
+            nombre: jugadorData.nombre,
+            edad: jugadorData.edad,
+            posicion: jugadorData.posicion,
+            nacionalidad: jugadorData.nacionalidad,
+            numeroCamiseta: jugadorData.numeroCamiseta,
+            id_equipo: new ObjectId(jugadorData.id_equipo),
+            id_lesion: jugadorData.id_lesion.map(id => new ObjectId(id)),
+            id_rendimiento: jugadorData.id_rendimiento.map(id => new ObjectId(id)),
+        };
+
+        const resultado = await this.collection.insertOne(nuevoJugador);
         await this.conexion.close();
-        return res;
+        return resultado.insertedId;
+    }
+
+    async editarJugador(id, jugadorData) {
+        await this.conexion.connect();
+        const resultado = await this.collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: {
+                nombre: jugadorData.nombre,
+                edad: jugadorData.edad,
+                posicion: jugadorData.posicion,
+                nacionalidad: jugadorData.nacionalidad,
+                numeroCamiseta: jugadorData.numeroCamiseta,
+                id_equipo: new ObjectId(jugadorData.id_equipo),
+                id_lesion: jugadorData.id_lesion.map(id => new ObjectId(id)),
+                id_rendimiento: jugadorData.id_rendimiento.map(id => new ObjectId(id))
+            } }
+        );
+        await this.conexion.close();
+        return resultado.modifiedCount;
+    }
+
+    async eliminarJugador(id) {
+        await this.conexion.connect();
+        const resultado = await this.collection.deleteOne({ _id: new ObjectId(id) });
+        await this.conexion.close();
+        return resultado.deletedCount;
     }
 }
