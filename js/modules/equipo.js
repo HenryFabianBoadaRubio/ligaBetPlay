da1bf8368f42e26b3f89d2b7cc0f4b7fdbec024c
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-21 23:53:04 -0500

    feat: :construction: Creacion del apartado de actualizacion así como algunos cambios en el registro
    Se generó el apartado de edicion, asi como la adicion del campo de id dentro de registro y a su vez dentro de edicion

--- js/modules/equipo.js
+++ js/modules/equipo.js
@@ -20,7 +20,7 @@ export class equipo extends connect {
         connect.instanceConnect = undefined;
     }
 
-    async registerTeam({ nombre, ciudad, id_estadio, id_entrenador, id_jugadores, id_partido }) {
+    async registerTeam({ _id, nombre, ciudad, id_estadio, id_entrenador, id_jugadores, id_partido }) {
         let res;
         try {
             // Verificar que el equipo no exista
@@ -34,6 +34,7 @@ export class equipo extends connect {
 
             // Inserción del equipo
             res = await this.collection.insertOne({
+                _id: _id ? new ObjectId(_id) : new ObjectId(),
                 nombre,
                 ciudad,
                 id_estadio: new ObjectId(id_estadio),
@@ -49,4 +50,32 @@ export class equipo extends connect {
             return { error: "Error", message: error.message, details: error.errInfo };
         }
     }
+
+    async updateTeam(id, data){
+        let res;
+        try {
+            const updateData = { ...data };
+            if (data.id_estadio) {
+                updateData.id_estadio = new ObjectId(data.id_estadio);
+            }
+            if (data.id_entrenador) {
+                updateData.id_entrenador = new ObjectId(data.id_entrenador);
+            }
+            if (data.id_jugadores) {
+                updateData.id_jugadores = data.id_jugadores.map(jugador => new ObjectId(jugador));
+            }
+            if (data.id_partido) {
+                updateData.id_partido = data.id_partido.map(partido => new ObjectId(partido));
+            }
+
+            res = await this.collection.updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+            return {
+                message: "Equipo actualizado correctamente",
+                data: res
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message, details: error.errInfo };
+        }
+    }
+
 }
