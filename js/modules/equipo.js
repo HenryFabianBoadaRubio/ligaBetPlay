5867c238beef9703a6e744c124960982ae3eabd5
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-21 23:19:46 -0500

    feat: :construction: cambios en la validacion de la existencia del equipo
    Se realizaron cambios en la validacion de la existncia del equipo, ya que se implementó dentro del registro de la misma

--- js/modules/equipo.js
+++ js/modules/equipo.js
@@ -1,11 +1,11 @@
 import { ObjectId } from 'mongodb';
 import { connect } from '../../helpers/db/connect.js';
 
+
 export class equipo extends connect {
     static instanceEquipo;
     db;
     collection;
-
     constructor() {
         if (equipo.instanceEquipo) {
             return equipo.instanceEquipo;
@@ -15,18 +15,38 @@ export class equipo extends connect {
         this.collection = this.db.collection('equipo');
         equipo.instanceEquipo = this;
     }
-
-    destructor() {
+    destructor(){
         equipo.instanceEquipo = undefined;
         connect.instanceConnect = undefined;
     }
 
-    async validarEquipo(nombre) {
-        await this.conexion.connect();
-        const equipoExistente = await this.collection.findOne({ nombre: nombre });
-        await this.conexion.close();
-        return equipoExistente;
+    async registerTeam({ nombre, ciudad, id_estadio, id_entrenador, id_jugadores, id_partido }) {
+        let res;
+        try {
+            // Verificar que el equipo no exista
+            const teamExist = await this.collection.findOne({ nombre: {$regex: new RegExp(`^${nombre}$`, 'i')} });
+            if (teamExist) {
+                return {
+                    error: "Not valid",
+                    message: "El equipo ya existe"
+                };
+            }
+
+            // Inserción del equipo
+            res = await this.collection.insertOne({
+                nombre,
+                ciudad,
+                id_estadio: new ObjectId(id_estadio),
+                id_entrenador: new ObjectId(id_entrenador),
+                id_jugadores: id_jugadores.map(jugador => new ObjectId(jugador)),
+                id_partido: id_partido.map(partido => new ObjectId(partido))
+            });
+            return {
+                message: "Equipo registrado correctamente",
+                data: res.ops
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message, details: error.errInfo };
+        }
     }
-
 }
-
