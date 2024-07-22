45f67f4b5fa7b464ee76316adb1b245b311ea08d
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-22 01:33:16 -0500

    feat: :construction: Creacion de registro de jugador
    Se generó el registro del jugador

--- js/modules/jugador.js
+++ js/modules/jugador.js
@@ -1,9 +1,11 @@
-import { connect } from "../../helpers/db/connect.js";
+import { ObjectId } from 'mongodb';
+import { connect } from '../../helpers/db/connect.js';
 
 export class jugador extends connect {
     static instanceJugador;
     db;
     collection;
+
     constructor() {
         if (jugador.instanceJugador) {
             return jugador.instanceJugador;
@@ -13,8 +15,36 @@ export class jugador extends connect {
         this.collection = this.db.collection('jugador');
         jugador.instanceJugador = this;
     }
-    destructor(){
+
+    destructor() {
         jugador.instanceJugador = undefined;
         connect.instanceConnect = undefined;
     }
+
+    async registrarJugador(jugadorData) {
+        await this.conexion.connect();
+        const jugadorExistente = await this.collection.findOne({ nombre: {$regex: new RegExp(`^${jugadorData.nombre}$`, i)}});
+        if (jugadorExistente) {
+            await this.conexion.close();
+            throw new Error("El jugador ya existe");
+        }
+
+        const nuevoJugador = {
+            _id: new ObjectId(jugadorData._id),
+            nombre: jugadorData.nombre,
+            edad: jugadorData.edad,
+            posicion: jugadorData.posicion,
+            nacionalidad: jugadorData.nacionalidad,
+            numeroCamiseta: jugadorData.numeroCamiseta,
+            id_equipo: new ObjectId(jugadorData.id_equipo),
+            id_lesion: jugadorData.id_lesion.map(id => new ObjectId(id)),
+            id_rendimiento: jugadorData.id_rendimiento.map(id => new ObjectId(id)),
+        };
+
+        const resultado = await this.collection.insertOne(nuevoJugador);
+        await this.conexion.close();
+        return resultado.insertedId;
+    }
+
+   
 }
