fd83b565bb9a306ca2d889e564a32f3f10b0503c
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-23 01:11:00 -0500

    feat: :construction: Avance en el caso de uso 8
    Se generaron avances en el caso de uso 8, en registro, actualizacion y eliminacion de un arbitro

--- js/modules/arbitro.js
+++ js/modules/arbitro.js
@@ -1,22 +1,85 @@
 import { connect } from "../../helpers/db/connect.js";
+import { ObjectId } from 'mongodb';
 
 // 8. Gestion de arbitros
 
-export class arbitro extends connect {
-    static instanceArbitro;
+export class ArbitroGestion extends connect {
+    static instanceArbitroGestion;
     db;
     collection;
+
     constructor() {
-        if (arbitro.instanceArbitro) {
-            return arbitro.instanceArbitro;
-        }
         super();
+        if (ArbitroGestion.instanceArbitroGestion) {
+            return ArbitroGestion.instanceArbitroGestion;
+        }
         this.db = this.conexion.db(this.getDbName);
         this.collection = this.db.collection('arbitro');
-        arbitro.instanceArbitro = this;
+        ArbitroGestion.instanceArbitroGestion = this;
     }
-    destructor(){
-        arbitro.instanceArbitro = undefined;
-        connect.instanceConnect = undefined;
+
+    destructor() {
+        ArbitroGestion.instanceArbitroGestion = undefined;
+        super.destructor();
+    }
+
+    async getAllTest() {
+        return await this.collection.find({}).toArray();
+    }
+
+    async registerReferee({ _id, id, nombre, edad, nacionalidad, experiencia, especialidad }) {
+        try {
+            const refereeExist = await this.collection.findOne({ id: new ObjectId(id) });
+            if (refereeExist) {
+                return {
+                    error: "Not valid",
+                    message: "El árbitro ya existe"
+                };
+            }
+
+            const res = await this.collection.insertOne({
+                _id: _id ? new ObjectId(_id) : new ObjectId(),
+                id: new ObjectId(id),
+                nombre,
+                edad,
+                nacionalidad,
+                experiencia,
+                especialidad
+            });
+            return {
+                message: "Árbitro registrado correctamente",
+                data: res.ops
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message, details: error.errInfo };
+        }
+    }
+
+    async updateReferee(id, data) {
+        try {
+            const updateData = { ...data };
+            if (data.id) {
+                updateData.id = new ObjectId(data.id);
+            }
+            const res = await this.collection.updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+            return {
+                message: "Árbitro actualizado correctamente",
+                data: res
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message, details: error.errInfo };
+        }
+    }
+
+    async deleteReferee(id) {
+        try {
+            const res = await this.collection.deleteOne({ _id: new ObjectId(id) });
+            return {
+                message: "Árbitro eliminado correctamente",
+                data: res
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message, details: error.errInfo };
+        }
     }
 }
