38bb7576bdb3783670cc9941ca966fc91bffa46d
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-23 00:32:38 -0500

    feat: :bookmark: Finalizacion de Caso de uso 8
    feat: :construction: Inicio caso de uso 8
    
    Se generó el inicio del caso de uso 8
    
    feat: :construction: Inicio del main del caso 8
    
    S generó el inicio en main del caso 8
    
    feat: :construction: Avance en el caso de uso 8
    
    Se generaron avances en el caso de uso 8, en registro, actualizacion y eliminacion de un arbitro
    
    feat: :sparkles: Finalizacion de caso de uso 8
    
    se generó la finalización del caso de uso 8
    
    docs: :memo: Documentacion de caso 8
    
    Se generó la documentacion del caso 8

--- js/modules/arbitro.js
+++ js/modules/arbitro.js
@@ -1,26 +1,134 @@
 import { connect } from "../../helpers/db/connect.js";
+import { ObjectId } from 'mongodb';
 
-export class arbitro extends connect {
-    static instanceArbitro;
+// 8. Gestion de arbitros
+
+export class ArbitroGestion extends connect {
+    static instanceArbitroGestion;
     db;
     collection;
+
     constructor() {
-        if (arbitro.instanceArbitro) {
-            return arbitro.instanceArbitro;
-        }
         super();
+        if (ArbitroGestion.instanceArbitroGestion) {
+            return ArbitroGestion.instanceArbitroGestion;
+        }
         this.db = this.conexion.db(this.getDbName);
         this.collection = this.db.collection('arbitro');
-        arbitro.instanceArbitro = this;
+        ArbitroGestion.instanceArbitroGestion = this;
     }
-    destructor(){
-        arbitro.instanceArbitro = undefined;
-        connect.instanceConnect = undefined;
+
+    destructor() {
+        ArbitroGestion.instanceArbitroGestion = undefined;
+        super.destructor();
     }
-    async getAllTest() {
-        await this.conexion.connect();
-        const res = await this.collection.find({}).toArray(); 
-        await this.conexion.close();
-        return res;
+
+    /**
+     * Registra un nuevo árbitro en la base de datos.
+     *
+     * @param {Object} datos - Los datos del árbitro a registrar.
+     * @param {string} datos._id - El identificador único para el árbitro. Si no se proporciona, se generará un nuevo ObjectId.
+     * @param {string} datos.id - El identificador único para el árbitro.
+     * @param {string} datos.nombre - El nombre del árbitro.
+     * @param {number} datos.edad - La edad del árbitro.
+     * @param {string} datos.nacionalidad - La nacionalidad del árbitro.
+     * @param {number} datos.experiencia - Los años de experiencia del árbitro.
+     * @param {string} datos.especialidad - La especialidad del árbitro.
+     *
+     * @returns {Object} - Un objeto que contiene el resultado de la operación.
+     * @returns {string} resultado.mensaje - Un mensaje que indica el éxito o fracaso de la operación.
+     * @returns {Object} resultado.datos - Los datos del árbitro si la operación fue exitosa.
+     * @returns {Object} resultado.error - Un objeto de error si la operación falló.
+     * @returns {string} resultado.error.error - El tipo de error.
+     * @returns {string} resultado.error.mensaje - El mensaje de error.
+     * @returns {Object} resultado.error.detalles - Detalles adicionales del error.
+     */
+    async registerReferee({ _id, id, nombre, edad, nacionalidad, experiencia, especialidad }) {
+        try {
+            const refereeExist = await this.collection.findOne({ id: new ObjectId(id) });
+            if (refereeExist) {
+                return {
+                    error: "No válido",
+                    mensaje: "El árbitro ya existe"
+                };
+            }
+
+            const res = await this.collection.insertOne({
+                _id: _id ? new ObjectId(_id) : new ObjectId(),
+                id: new ObjectId(id),
+                nombre,
+                edad,
+                nacionalidad,
+                experiencia,
+                especialidad
+            });
+            return {
+                mensaje: "Árbitro registrado correctamente",
+                datos: res.ops
+            };
+        } catch (error) {
+            return { error: "Error", mensaje: error.message, detalles: error.errInfo };
+        }
+    }
+
+    /**
+     * Actualiza los datos de un árbitro en la base de datos.
+     *
+     * @param {string} id - El identificador único del árbitro a actualizar.
+     * @param {Object} data - Los nuevos datos del árbitro.
+     * @param {string} [data.id] - El nuevo identificador único para el árbitro.
+     * @param {string} [data.nombre] - El nuevo nombre del árbitro.
+     * @param {number} [data.edad] - La nueva edad del árbitro.
+     * @param {string} [data.nacionalidad] - La nueva nacionalidad del árbitro.
+     * @param {number} [data.experiencia] - Los nuevos años de experiencia del árbitro.
+     * @param {string} [data.especialidad] - La nueva especialidad del árbitro.
+     *
+     * @returns {Promise} - Una promesa que resuelve con un objeto que contiene el resultado de la operación.
+     * @returns {string} result.message - Un mensaje que indica el éxito o fracaso de la operación.
+     * @returns {Object} result.data - Los detalles de la operación de actualización.
+     * @returns {Object} result.error - Un objeto de error si la operación falló.
+     * @returns {string} result.error.error - El tipo de error.
+     * @returns {string} result.error.message - El mensaje de error.
+     * @returns {Object} result.error.details - Detalles adicionales del error.
+     */
+    async updateReferee(id, data) {
+        try {
+            const updateData = { ...data };
+            if (data.id) {
+                updateData.id = new ObjectId(data.id);
+            }
+            const res = await this.collection.updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+            return {
+                message: "Árbitro actualizado correctamente",
+                data: res
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message, details: error.errInfo };
+        }
+    }
+
+    /**
+     * Elimina un árbitro de la base de datos por su identificador único.
+     *
+     * @param {string} id - El identificador único del árbitro a eliminar.
+     *
+     * @returns {Promise} - Una promesa que resuelve con un objeto que contiene el resultado de la operación.
+     * @returns {string} result.message - Un mensaje que indica el éxito o fracaso de la operación.
+     * @returns {Object} result.data - Los detalles de la operación de eliminación.
+     * @returns {Object} result.error - Un objeto de error si la operación falló.
+     * @returns {string} result.error.error - El tipo de error.
+     * @returns {string} result.error.message - El mensaje de error.
+     * @returns {Object} result.error.details - Detalles adicionales del error.
+     */
+    async deleteReferee(id) {
+        try {
+            const res = await this.collection.deleteOne({ _id: new ObjectId(id) });
+            return {
+                message: "Árbitro eliminado correctamente",
+                data: res
+            };
+        } catch (error) {
+            return { error: "Error", message: error.message, details: error.errInfo };
+        }
     }
 }
